cc8bf133e773b70095b2397d49a1cb7d4bc98175
ciaochaos <1272777550@qq.com>
2023-07-28 14:31:14 +0800

    1

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import asyncio
+
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 
 app = FastAPI()
 
+
+async def fake_video_streamer():
+    for letter in "some fake video bytes some fake video bytes":
+        yield letter
+        await asyncio.sleep(0.1)
+
+
 @app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+async def main():
+    return StreamingResponse(fake_video_streamer(), media_type="application/json", status_code=200)
+
+
